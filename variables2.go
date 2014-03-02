059c211ac4c77f26d6f5d09447fcb20cbd21bd08
occamshatchet <bentinney@gmail.com>
2014-03-01 23:41:19 -0500

    testing

--- variables2.go
+++ variables2.go
@@ -2,3 +2,24 @@ package main
 
 import "fmt"
 
+func main() {
+
+    // var declars 1 or more variables.
+    var a string = "initial"
+    fmt.Println(a)
+
+    // You can declare multiple variables at once.
+    var a, b int = 1, 2
+
+    // go will infer the type of initialized variables.
+    var d = true
+    fmt.Println(d)
+
+    // variables without initialization are zero values. 
+    var e int
+    fmt.Println(e)
+
+    // := is shorthand for declaring and initalizing a variable.
+    f := "short"
+    fmt.Println(f)
+}
